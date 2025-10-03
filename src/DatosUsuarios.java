5ef907169490e63c8bbbd5d6bfc17438110f6454
Andres-2334 <777@alumnos.fpsumma.es>
2025-10-03 14:14:33 +0200

    nose

--- src/DatosUsuarios.java
+++ src/DatosUsuarios.java
@@ -6,20 +6,17 @@ public class DatosUsuarios {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("introduce tu nombre: ");
         String Nombre = sc.nextLine();
-        int Edad = sc.nextInt();
+        System.out.println("introduce tu edad: ");
+        int Edad = sc.nextInt(); sc.nextLine();
+        System.out.println("Introduce tu correo: ");
         String correo = sc.nextLine();
         boolean emailValido = ProcesadorTexto.esEmailValido(correo);
-        if emailValido{
+        if (emailValido){
             System.out.println(correo + "Registrado");
         } else { 
-            System.out.println("e");
+            System.out.println("Error al registrar correo invalido ");
         }
-
-
-         
-        
-
-
     }
 }
